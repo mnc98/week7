6bc3aa4807d41218c3bf560e37a6abea8c08edc8
Michael N Cohen <mnc24@afsconnect1.njit.edu>
2017-10-22 12:42:47 -0400

    added html table

---
+++ show.php
@@ -0,0 +1,15 @@
+<?php
+require 'pdoconnect.php' ;
+$sql = "select id,email,fname,password from accounts where id<6  ";
+$results = runQuery($sql);
+if(count($results) > 0)
+{
+	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th><th>First Name</th><th>Pass</th></tr>";
+	foreach ($results as $row) {
+		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["password"]."</td></tr>";
+	}
+	
+}else{
+    echo '0 results';
+}
+?>
